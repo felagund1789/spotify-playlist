961040e41b6a59c95cbdee68c2ec30d65d26eaf1
Konstantinos K <raistlindragonl@gmail.com>
2024-07-24 21:04:22 +0300

    Add dummy components and change page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid, GridItem } from "@chakra-ui/react";
+import "./App.css";
+import SearchResults from "./components/SearchResults";
+import SearchInput from "./components/SearchInput";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Grid
+      templateAreas={{
+        base: `"search" "results" "playlist"`,
+        lg: `"search results playlist"`,
+      }}
+      templateColumns={{
+        base: "1fr 1fr 1fr",
+        lg: "1fr 1fr 1fr",
+      }}
+    >
+      <GridItem padding={10} area="search">
+        <SearchInput onSearch={(search) => console.log(search)} />
+      </GridItem>
+      <GridItem padding={10} area="results">
+        <SearchResults />
+      </GridItem>
+      <GridItem padding={10} area="playlist">
+        <div />
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,20 @@
+import { List, ListItem } from "@chakra-ui/react";
+
+const SearchResults = () => {
+  return (
+    <List>
+      <ListItem>Item 1</ListItem>
+      <ListItem>Item 2</ListItem>
+      <ListItem>Item 3</ListItem>
+      <ListItem>Item 4</ListItem>
+      <ListItem>Item 5</ListItem>
+      <ListItem>Item 6</ListItem>
+      <ListItem>Item 7</ListItem>
+      <ListItem>Item 8</ListItem>
+      <ListItem>Item 9</ListItem>
+      <ListItem>Item 10</ListItem>
+    </List>
+  );
+};
+
+export default SearchResults;
